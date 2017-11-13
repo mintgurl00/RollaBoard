bef042e503df30adadcfb1935bc9263962bacb93
SOOMINAHN <sm921125@gmail.com>
2017-11-13 17:10:08 +0900

    VO 클래스

---
+++ RollaBoard/src/main/java/com/spring/rollaboard/SectionVO.java
@@ -0,0 +1,28 @@
+package com.spring.rollaboard;
+
+public class SectionVO {
+	
+	private int id;
+	private int board_id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
