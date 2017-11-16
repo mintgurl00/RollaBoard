a9f43a6635451d15a016f2ec6c4b50358a62fcf5
석원 <@kosa-PC>
2017-11-16 09:08:25 +0900

    dddd

--- RollaBoard/src/main/java/com/spring/rollaboard/BoardMemMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.rollaboard;
-
-public interface BoardMemMapper {
-
-	public int joinBoardChk(int board_id, String mem_id);
-	
-	public void joinBoard(int board_id, String mem_id);
-	
-}
